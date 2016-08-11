badd9caaeab2111f28e9d547a47d0401dccd663b
Jason Song <wolfogre@outlook.com>
2016-08-11 17:12:09 +0800

    连上Sqlite，吃饭！

--- SqliteToExcel/Program.cs
+++ SqliteToExcel/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,16 @@ namespace SqliteToExcel
 	{
 		static void Main(string[] args)
 		{
+			SQLiteConnection conn = new SQLiteConnection("Data Source = weather.db"); ;
+			conn.Open();
+			SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter("select * from nasaweather", conn);
+			DataSet dataSet = new DataSet();
+			dataAdapter.Fill(dataSet, "nasaweather");
+			DataTable dataTable = dataSet.Tables["nasaweather"];
+			foreach (DataRow dataRow in dataTable.Rows)
+			{
+				Console.WriteLine(dataRow["lat"] + " " + dataRow["lon"] + " " + dataRow["type"] + " " + dataRow["data"]);
+			}
 		}
 	}
 }
